46e5360a474ccde4820ee2d705b44eff5eb20846
Neon Riot <sbrookshire99@gmail.com>
2021-05-11 22:56:52 -0500

    Built out questions and prompt generator with inquirer

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [
+  "What is your GitHub username?",
+  "What is your email?",
   "What is the title of your project?",
-  "Give a brief description of the project",
+  "Give a brief description of the project:",
   "What license do you want for your project?",
+  "What commands are needed to install dependencies?",
+  "What commands are needed to run tests?",
+  "What should other users know about the repo?",
+  "Enter your message on contribution:",
 ];
 
 function collectData() {
@@ -15,18 +21,42 @@ function collectData() {
   inquirer
     .prompt([
       {
-        name: "projectTitle",
+        name: "github",
         message: questions[0],
       },
       {
-        name: "projectDesc",
+        name: "email",
         message: questions[1],
+      },
+      {
+        name: "projectTitle",
+        message: questions[2],
+      },
+      {
+        name: "projectDesc",
+        message: questions[3],
       },
       {
         type: "list",
         name: "projectLic",
-        message: questions[2],
-        choices: ["MIT", "Open-Source", "Open Commons"],
+        message: questions[4],
+        choices: ["MIT", "Open-Source", "Community License", "GNU GPLv3"],
+      },
+      {
+        name: "dependencies",
+        message: questions[5],
+      },
+      {
+        name: "tests",
+        message: questions[6],
+      },
+      {
+        name: "repoInfo",
+        message: questions[7],
+      },
+      {
+        name: "contributionInfo",
+        message: questions[8],
       },
     ])
     .then((answers) => {
